62bca6f61ea6ed781b02f72842c321264f0e51ac
Allen Russell <garyrusselljr@gmail.com>
2024-01-26 11:00:16 -0700

    feat: crud functionality for appointments complete

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -20,4 +20,47 @@ class Api::V1::AppointmentsController < ApplicationController
       render json: { errors: e.message }, status: :not_found
     end
   end  
+
+  def create
+    begin
+      patient = Patient.find(params[:patient_id])
+
+      patient.appointments.create(appointment_params)
+      render json: { message: "Appointment created successfully" }, status: :created
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  def edit; end
+
+  def update
+    begin 
+      patient = Patient.find(params[:patient_id])
+      appointment = patient.appointments.find(params[:id])
+      
+      appointment.update(appointment_params)
+      render json: { message: "Appointment updated successfully" }, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  def destroy
+    begin
+      patient = Patient.find(params[:patient_id])
+      appointment = patient.appointments.find(params[:id])
+
+      appointment.destroy
+      render json: { message: "Appointment deleted successfully" }, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :description, :start_time, :end_time, :patient_id)
+  end
 end
