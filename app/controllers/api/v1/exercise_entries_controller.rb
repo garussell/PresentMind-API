9e3bcb4085758ee45ffa906a5edb9c1de4cf8a5d
Allen Russell <garyrusselljr@gmail.com>
2024-01-29 12:55:36 -0700

    feat: exercise_entry crud complete

---
+++ app/controllers/api/v1/exercise_entries_controller.rb
@@ -0,0 +1,64 @@
+class Api::V1::ExerciseEntriesController < ApplicationController
+  before_action :authorize
+
+  def index
+    begin
+      entries = Patient.find(params[:patient_id]).exercise_entries
+      render json: { exercise_entries: entries, status: :ok }
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  def show
+    begin 
+      entry = Patient.find(params[:patient_id]).exercise_entries.find(params[:id])
+      render json: { exercise_entry: entry, status: :ok }
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  def create
+    begin 
+      patient = Patient.find(params[:patient_id])
+
+      patient.exercise_entries.create(exercise_entry_params)
+      render json: { message: "Exercise entry created successfully" }, status: :created
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  def edit; end
+
+  def update  
+    begin
+      patient = Patient.find(params[:patient_id])
+      entry = patient.exercise_entries.find(params[:id])
+
+      entry.update(exercise_entry_params)
+      render json: { message: "Exercise entry updated successfully" }, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  def destroy
+    begin
+      patient = Patient.find(params[:patient_id])
+      entry = patient.exercise_entries.find(params[:id])
+
+      entry.destroy
+      render json: { message: "Exercise entry deleted successfully" }, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :not_found
+    end
+  end
+
+  private
+
+  def exercise_entry_params
+    params.require(:exercise_entry).permit(:goal, :exercise_type, :total_minutes, :date, :patient_id)
+  end
+end
--- app/models/exercise_entry.rb
+++ app/models/exercise_entry.rb
@@ -7,5 +7,10 @@ class ExerciseEntry < ApplicationRecord
                         :date, 
                         :patient_id
 
+  # Categories are based on the 'exercises' endpoint from API Ninjas
+  # As a separate but related feature, when a user selects an exercise type,
+  # the app will display a list of exercises that match the selected type.
+  # The user can then select an exercise from the list which will be accompanied
+  # by instructions and other data from the api response.
   enum exercise_type: [:cardio, :olympic_weightlifting, :plyometrics, :powerlifting, :strength, :stretching, :strongman]
 end
