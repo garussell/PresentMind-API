22c82b567b7961fbc2333d26ed6bbe91bff93379
Allen Russell <garyrusselljr@gmail.com>
2024-01-29 16:25:18 -0700

    fix: remove notes and date from model

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -3,10 +3,7 @@ class Mood < ApplicationRecord
   
   validates_presence_of :current_mood_scale, 
                         :stress_level_scale, 
-                        :notes, 
-                        :date, 
                         :patient_id
-  
 
   enum current_mood_scale: { very_negative: 1, moderately_negative: 2, slightly_negative: 3, neutral: 4, slightly_positive: 5, moderately_positive: 6, very_positive: 7 }
   enum stress_level_scale: { very_stressed: 1, moderately_stressed: 2, slightly_stressed: 3, moderate: 4, slightly_relaxed: 5, moderately_relaxed: 6, very_relaxed: 7 }
